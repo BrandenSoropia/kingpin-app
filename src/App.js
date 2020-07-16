3a54b5ffe4695e484c944a4e35fd25c239927cc3
BrandenSoropia <b.soropia@gmail.com>
2020-07-16 09:29:14 -0400

    Setup React Native Navigation.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 import StorybookUI from "./storybook";
 import { ThemeProvider } from "styled-components";
 import { registerRootComponent } from "expo";
 import { theme, Flex } from "ui-kit";
+import { NavigationContainer } from "@react-navigation/native";
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Flex flex={1}>
-        <StatusBar style="auto" />
-        <StorybookUI />
-      </Flex>
-    </ThemeProvider>
+    <NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <Flex flex={1}>
+          <StatusBar style="auto" />
+          <StorybookUI />
+        </Flex>
+      </ThemeProvider>
+    </NavigationContainer>
   );
 };
 
