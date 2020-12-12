818568db11d9387f4a9ca6f726873d973864713b
BrandenSoropia <b.soropia@gmail.com>
2020-12-12 14:21:53 -0500

    Upgrade Storybook.

--- src/storybook/storyLoader.js
+++ src/storybook/storyLoader.js
@@ -4,27 +4,27 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-	require('../Profile/Edit/Edit.stories');
-	require('../Profile/Profile.stories');
-	require('../Profile/components/Details.stories');
-	require('../SkateparkFinder/SkateparkDetails/SkateparkDetails.stories');
-	require('../SkateparkFinder/SkateparkFinder.stories');
-	require('../SkateparkFinder/components/SkateparkListItem.stories');
-	require('../TrickTracker/TrickDetails/TrickDetails.stories');
-	require('../TrickTracker/TrickTracker.stories');
-	require('../TrickTracker/components/TrickListItem.stories');
+  require("../Profile/components/Details.stories");
+  require("../Profile/Edit/Edit.stories");
+  require("../Profile/Profile.stories");
+  require("../SkateparkFinder/components/SkateparkListItem.stories");
+  require("../SkateparkFinder/SkateparkDetails/SkateparkDetails.stories");
+  require("../SkateparkFinder/SkateparkFinder.stories");
+  require("../TrickTracker/components/TrickListItem.stories");
+  require("../TrickTracker/TrickDetails/TrickDetails.stories");
+  require("../TrickTracker/TrickTracker.stories");
 }
 
 const stories = [
-	'../Profile/Edit/Edit.stories',
-	'../Profile/Profile.stories',
-	'../Profile/components/Details.stories',
-	'../SkateparkFinder/SkateparkDetails/SkateparkDetails.stories',
-	'../SkateparkFinder/SkateparkFinder.stories',
-	'../SkateparkFinder/components/SkateparkListItem.stories',
-	'../TrickTracker/TrickDetails/TrickDetails.stories',
-	'../TrickTracker/TrickTracker.stories',
-	'../TrickTracker/components/TrickListItem.stories'
+  "../Profile/components/Details.stories",
+  "../Profile/Edit/Edit.stories",
+  "../Profile/Profile.stories",
+  "../SkateparkFinder/components/SkateparkListItem.stories",
+  "../SkateparkFinder/SkateparkDetails/SkateparkDetails.stories",
+  "../SkateparkFinder/SkateparkFinder.stories",
+  "../TrickTracker/components/TrickListItem.stories",
+  "../TrickTracker/TrickDetails/TrickDetails.stories",
+  "../TrickTracker/TrickTracker.stories",
 ];
 
 module.exports = {
